14db6337e8757d9ec4fe03531f3b04a79fe40464
yelnik89 <denis.yelnik@icloud.com>
2020-10-01 17:47:42 +0700

    edit Loger: add oldLog direct and move old log in

--- roboProg/Loger.cs
+++ roboProg/Loger.cs
@@ -10,12 +10,17 @@ namespace roboProg
 {
     class Loger
     {
-        private const string LOGFILE = "log.txt";
+        private const string LOGFOLDER = "logs";
+        private const string OLDLOGSFOLDER = "old_logs";
+        private const string FILENAME = "_log";
+        private const string FORMAT = ".txt";
 
         private static Loger loger;
 
         private Loger()
-        { }
+        {
+            prepareLogsFolders();
+        }
 
         public static Loger getInstance()
         {
@@ -27,11 +32,16 @@ namespace roboProg
 
         public void writeLog(string text)
         {
-            using (FileStream file = new FileStream(LOGFILE, FileMode.Append))
+            using (FileStream file = new FileStream(LOGFOLDER + "/" + logFile(), FileMode.Append))
             {
                 byte[] array = prepareText(text);
                 file.Write(array, 0, array.Length);
             }
+        }
+
+        private string logFile()
+        {
+            return DateTime.Today.ToShortDateString() + FILENAME + FORMAT;
         }
 
         private byte[] prepareText(string text)
@@ -40,6 +50,30 @@ namespace roboProg
             s += ": " + text + '\n';
             byte[] array = Encoding.Default.GetBytes(s);
             return array;
+        }
+
+        private void prepareLogsFolders()
+        {
+            checkAndCreateLogsDirectory();
+            moveOldLogs();
+        }
+
+        private void checkAndCreateLogsDirectory()
+        {
+            DirectoryInfo logsDirInfo = new DirectoryInfo(LOGFOLDER);
+            DirectoryInfo oldLogsDirInfo = new DirectoryInfo(LOGFOLDER + "/" + OLDLOGSFOLDER);
+            if (!logsDirInfo.Exists) logsDirInfo.Create();
+            if (!oldLogsDirInfo.Exists) oldLogsDirInfo.Create();
+        }
+
+        private void moveOldLogs()
+        {
+            DirectoryInfo logsDirInfo = new DirectoryInfo(LOGFOLDER);
+            FileInfo[] logsFiles = logsDirInfo.GetFiles();
+            foreach (FileInfo file in logsFiles)
+            {
+                if (!file.Name.Equals(logFile())) file.MoveTo(LOGFOLDER + "/" + OLDLOGSFOLDER + "/" + file.Name);
+            }
         }
     }
 }
